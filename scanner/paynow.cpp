25a069cce9bb6e41f5b5026cb69aa80f93f4efa0
Leslie Yip <leslieyip02@gmail.com>
2024-07-19 00:42:43 +0800

    Refactor to save memory

--- scanner/paynow.cpp
+++ scanner/paynow.cpp
@@ -24,24 +24,13 @@ char* generateCode(double amount) {
     char* message = (char*) malloc(16 * sizeof(char));
     sprintf(message, "01%02d%s", strlen(PAYNOW_MESSAGE), PAYNOW_MESSAGE);
 
-    strcpy(payload, "000201");          // format
-    strcat(payload, "010211");          // method (11 for static, 12 for dynamic)
-
-    strcat(payload, "2638");            // merchant account information header (38 is the length of the body)
-    strcat(payload, "0009SG.PAYNOW");   // reverse domain name
-    strcat(payload, "01010");           // proxy type (0 for mobile number, 2 for UEN)
-    strcat(payload, proxyValue);        // proxy value (+65XXXXXXXX)
-    strcat(payload, "03011");           // editability (0 for no, 1 for yes)
-
-    strcat(payload, "52040000");        // merchant category
-    strcat(payload, "5303702");         // currency (702 is the ISO4217 code for SGD)
-    strcat(payload, roundedAmount);     // amount (2dp)
-    strcat(payload, "5802SG");          // country code
-    strcat(payload, "5902NA");          // merchant name
-    strcat(payload, "6009Singapore");   // city
-    strcat(payload, "6211");            // message header (11 is the length of the body)
-    strcat(payload, message);           // message
-    strcat(payload, "6304");            // checksum indicator
+    strcat(payload, BLOCK_1);       // predefined to save memory
+    strcat(payload, proxyValue);    // proxy value (+65XXXXXXXX)
+    strcat(payload, BLOCK_2);       // predefined to save memory
+    strcat(payload, roundedAmount); // amount (2dp)
+    strcat(payload, BLOCK_3);       // predefined to save memory
+    strcat(payload, message);       // message
+    strcat(payload, "6304");        // checksum indicator
 
     char* checksum = generateChecksum(payload);
     strcat(payload, checksum);
--- scanner/paynow.h
+++ scanner/paynow.h
@@ -1,6 +1,25 @@
 #ifndef PAYNOW_H
 #define PAYNOW_H
 
+// 000201           // format
+// 010211           // method (11 for static, 12 for dynamic)
+// 2638             // merchant account information header (38 is the length of the body)
+// 0009SG.PAYNOW    // reverse domain name
+// 01010            // proxy type (0 for mobile number, 2 for UEN)
+#define BLOCK_1 "00020101021126380009SG.PAYNOW01010"
+
+// 03011            // editability (0 for no, 1 for yes)
+// 52040000         // merchant category
+// 5303702          // currency (702 is the ISO4217 code for SGD)
+#define BLOCK_2 "03011520400005303702"
+
+// roundedAmount    // amount (2dp)
+// 5802SG           // country code
+// 5902NA           // merchant name
+// 6009Singapore    // city
+// 6211             // message header (11 is the length of the body)
+#define BLOCK_3 "5802SG5902NA6009Singapore6211"
+
 extern char* generateCode(double amount);
 extern char* generateChecksum(char* payload);
 
